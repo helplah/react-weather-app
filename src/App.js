a8463b4c26ed0a1b94cb25f23cfb6ac6c39de1fa
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-19 07:35:56 +0800

    Initial commit of completed react-weather-app instructor created to showcase react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import isEmpty from "lodash.isempty";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function App() {
+const Weather = ({ match, forecast }) => {
+  const weatherId = Number(match.params.weatherId);
+  const weather = forecast.find(w => w.id === weatherId);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Today's Weather</h1>
+      <h2>{weather.weather_state_name}</h2>
+    </React.Fragment>
   );
+};
+
+Weather.propTypes = {
+  forecast: PropTypes.array.isRequired
+};
+
+const NavBar = ({ forecast }) => {
+  return (
+    <nav>
+      {forecast.map(w => {
+        return (
+          <li key={w.id}>
+            <Link to={`/weather/${w.id}`}>{w.applicable_date}</Link>
+          </li>
+        );
+      })}
+    </nav>
+  );
+};
+
+NavBar.propTypes = {
+  forecast: PropTypes.array.isRequired
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forecast: []
+    };
+  }
+
+  componentDidMount() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api = "https://www.metaweather.com/api/location/1062617/";
+    const url = proxy + api;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          forecast: data.consolidated_weather
+        });
+      });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavBar forecast={this.state.forecast} />
+          {!isEmpty(this.state.forecast) && (
+            <main>
+              <Route
+                path="/weather/:weatherId"
+                render={props => {
+                  return <Weather forecast={this.state.forecast} {...props} />;
+                }}
+              />
+            </main>
+          )}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
